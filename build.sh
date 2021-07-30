85210fc18e9b9612944289887ce24ea05ec47bad
Henrylle Maia <henryllemaia@gmail.com>
2021-07-30 17:04:05 -0300

    novos ajustes para o evento

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t imersao-aws .
-aws ecr get-login-password --region us-east-1  --profile imersaoawstreino | docker login --username AWS --password-stdin 053481394261.dkr.ecr.us-east-1.amazonaws.com
-docker tag imersao-aws:latest 053481394261.dkr.ecr.us-east-1.amazonaws.com/imersaoawstreino:latest
-docker push 053481394261.dkr.ecr.us-east-1.amazonaws.com/imersaoawstreino:latest
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-./build.sh
-aws ecs update-service --cluster cluster-imersao-aws-treino --service service-imersao-aws-treino  --force-new-deployment --profile imersaoawstreino
